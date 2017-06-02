608f82b5d1163bbfd5eb9b546c43959724089278
Sean Ramzan <sramzan16@gmail.com>
2017-06-02 19:58:51 -0400

    Final cleanup

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -32,8 +32,8 @@ router.get('/stemmer', function(req, res, next) {
   try {
     var wordListRequest = request(url, function(error, response, body){
       console.log('Word List acquired');
-      // var wordList = body.split(delim);
-      var wordList = ['preface','face', 'prefaced', 'walk','walking','fetch','bearable','enlighten', null, undefined, '', ' ', 'forcemeat', 'speedometer', 'force', 'meat', 'speed', 'meter', 'forcemeat', 'speedometer', 'force'];
+      var wordList = body.split(delim);
+      // var wordList = ['preface','face', 'prefaced', 'walk','walking','fetch','bearable','enlighten', null, undefined, '', ' ', 'forcemeat', 'speedometer', 'force', 'meat', 'speed', 'meter', 'forcemeat', 'speedometer', 'force'];
       console.log('Word List Provided: ' + wordList);
       var stemEngine = new stemUtil.StemEngine(wordList, 'en');
       results = stemEngine.stemWordList();
